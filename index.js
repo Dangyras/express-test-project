cb6c2ffa6d2b4c34e3791ec87e88ebe776d80123
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-08-05 18:15:40 +0300

    Added GET requst method.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const path = require("path");
+const projects = require("./Projects");
 
 const app = express();
+
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //     res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
+
+app.get("/api/projects", (req, res) => {
+    res.json(projects);
+});
+
+app.get("/projects/:title", (req, res) => {
+    res.json(projects.filter(project => project.title === req.params.title));
+});
 
 app.use(
     express.static(path.join(__dirname, "public"), {
