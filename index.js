44ba825f6f177d5936436671e2dfcc15ca5f8205
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-08-05 15:52:36 +0300

    Added static folder.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+app.use(
+    express.static(path.join(__dirname, "public"), {
+        extensions: ["html", "htm"]
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 
