c5e21c8f747af0f8808043061cc1da3ff9476812
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2019-08-05 18:14:29 +0300

    Added some api content.

---
+++ Projects.js
@@ -0,0 +1,29 @@
+const projects = [
+    {
+        id: 1,
+        title: "project_1",
+        type: "PHP"
+    },
+    {
+        id: 2,
+        title: "project_2",
+        type: "PHP"
+    },
+    {
+        id: 3,
+        title: "project_3",
+        type: "PHP"
+    },
+    {
+        id: 4,
+        title: "project_4",
+        type: "PHP"
+    },
+    {
+        id: 5,
+        title: "project_5",
+        type: "PHP"
+    }
+];
+
+module.exports = projects;
